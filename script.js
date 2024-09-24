1e77d8f45c2929fe65bb5ffd922ae93a25f6b762
Sarahwcode <sarahw181090@gmail.com>
2024-09-24 17:21:55 +0100

    Added axios and javascript

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-function cityChanges(event) {
+function cityChanges(response) {
+  let cityData = response.data.city;
+  let cityDisplay = document.querySelector("#city-change");
+  cityDisplay.innerHTML = cityData;
+  let weather = response.data;
+  let temperature = Math.round(weather.temperature.current);
+  let tempChange = document.querySelector("#temp-change");
+  tempChange.innerHTML = `${temperature}°C`;
+}
+function getCity(event) {
   event.preventDefault();
-  let inputCity = document.querySelector("#input-city");
-  let citySubmit = document.querySelector("#city-Change");
-  inputCity.innerHTML = citySubmit.value;
+  let inputCity = document.querySelector("#city-input").value;
+  let apiKey = "145ba0dc50b14bt75oafffd6a19b4a36";
+  let url = `https://api.shecodes.io/weather/v1/current?query=${inputCity}&key=${apiKey}&units=metric`;
+  axios.get(url).then(cityChanges);
 }
 
 let citySearch = document.querySelector("#city-search");
-citySearch.addEventListener("submit", cityChanges);
+citySearch.addEventListener("submit", getCity);
